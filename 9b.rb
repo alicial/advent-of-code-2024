ba5dcdef87ef13c50e7edc33c9a44ad67f1a4c95
Alicia Liu <xalicia@gmail.com>
2024-12-09 21:42:04 -0800

    day 9 part 2

--- 9b.rb
+++ 9b.rb
@@ -2,45 +2,83 @@ require 'benchmark'
 
 MyFile = Data.define(:id, :index, :size)
 
+class MyDisk
+    attr_accessor :blocks, :files
+
+    def self.create(str)
+        disk = MyDisk.new
+        fi = 0
+        str.chars.map(&:to_i).each_with_index do |size, i|
+            f_id = i % 2 == 0 ? i/2 : nil
+            disk.blocks += [f_id] * size
+            disk.files << MyFile.new(f_id, fi, size) if !f_id.nil?
+            fi += size
+        end
+        disk
+    end
+
+    def initialize
+        @blocks = []
+        @files = []
+    end
+
+    def defrag
+        files.reverse.each do |file| 
+            index = find_space(file)
+            move_file_to_index(file, index) if !index.nil?
+        end
+    end
+
+    def move_file_to_index(file, index)
+        (0..file.size-1).each do |i|
+            blocks[index + i] = file.id # write file to new location
+            blocks[file.index + i] = nil # null out previous location
+        end
+    end
+
+    def find_space_size(index)
+        size = 0
+        while blocks[index].nil? && index < blocks.size do
+            size += 1
+            index += 1
+        end
+        size
+    end
+
+    def find_next_space(index, end_index)
+        i = blocks[index..end_index].index(&:nil?)
+        
+        if i.nil?
+            [nil, 0]
+        else
+            [index + i, find_space_size(index + i)]
+        end
+    end
+
+    def find_space(file)
+        i = 0
+        loop do
+            next_space, space_size = find_next_space(i, file.index)
+            break if next_space.nil?
+            
+            return next_space if space_size >= file.size
+            i = next_space + space_size
+        end
+        nil
+    end
+
+    def checksum
+        blocks.each_with_index.reduce(0) { |sum, (f_id, i)| sum + (f_id ? f_id * i : 0) }
+    end
+end
+
 input = IO.readlines(ARGV[0]).first.chomp
 
-result = []
-fi = 0
-files = [] #create an index of files so they're easier to iterate through
-
-input.split("").map(&:to_i).each_with_index do |size, i|
-    f_id = i % 2 == 0 ? i/2 : nil
-    result += [f_id] * size
-    files << MyFile.new(f_id, fi, size) if !f_id.nil?
-    fi += size
-end
-
-def find_space(arr, file)
-    i = arr.index(&:nil?)
-    while i < file.index do
-        available = arr[i..-1].index{|x| !x.nil?}
-        break if available.nil?
-
-        if available >= file.size
-            (0..file.size-1).each do |j|
-                arr[i+j] = file.id # write file to new location
-                arr[file.index + j] = nil # null out previous location
-            end
-            return true
-        end
-
-        next_space = arr[i+available..-1].index(&:nil?)
-        break if next_space.nil?
-        i += available + next_space
-    end
-    false
-end
+disk = MyDisk.create(input)
 
 puts Benchmark.measure {
 
-(files.size-1).step(0, -1) { |i| find_space(result, files[i]) }
-
-# calculate checksum
-puts result.each_with_index.reduce(0) { |sum, (f_id, i)| sum + (f_id ? f_id * i : 0) }
+disk.defrag
+puts disk.checksum
 
 }
