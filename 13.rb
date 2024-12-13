4c70d3ce841e698aa1cc9a64ed1349ac3e0ab474
Alicia Liu <xalicia@gmail.com>
2024-12-13 01:01:31 -0800

    remove recursion + part 2

--- 13.rb
+++ 13.rb
@@ -3,43 +3,46 @@ Machine = Struct.new('Machine', :ax, :ay, :bx, :by, :x, :y) do
   COST_B = 1
   MAX_PUSHES = 100
 
-  def pushA(dx, dy)
-    if dx % ax == 0 && dy % ay == 0
-      return (dx / ax) * COST_A
-    end 
+  def solve_equations
+    det = ax * by - ay * bx
+    return nil if det == 0
+
+    n = by * x - bx * y
+    m = -ay * x + ax * y
+
+    [n / det, m / det] if n % det == 0 && m % det == 0
   end
 
-  def pushB(dx, dy)
+  def solve
+    n, m = solve_equations
+    n && m ? n * COST_A + m * COST_B : 0
+  end
+
+  def pushA(cost, dx, dy)
+    [cost + COST_A, dx - ax, dy - ay]
+  end
+
+  def pushB(cost, dx, dy)
     if dx % bx == 0 && dy % by == 0 && dx / bx == dy / by
-      return (dx / bx)
+      b_pushes = (dx/bx)
+      return cost + b_pushes * COST_B
     end
     nil
   end
 
-  def iterate(cost, dx, dy, lowest_cost, num_pushes)
-    return lowest_cost if num_pushes > MAX_PUSHES || dx.negative? || dy.negative?
-
-    # weightA = ((dx / ax) + (dy / ay)) * COST_A
-    # weightB = (dx / bx) + (dy / by)
-
-    # added_cost = weightA > weightB ? pushB(dx, dy) : pushA(dx, dy)
-
-    pushes = pushB(dx, dy)
-    if pushes && pushes <= MAX_PUSHES
-      # puts "FOUND #{i}: #{cost} + #{pushes} = #{cost + pushes * COST_B}, #{lowest_cost}"
-      lowest_cost = [cost + pushes * COST_B, lowest_cost || Float::INFINITY].min
+  def solve_original
+    cost = 0
+    dx = x
+    dy = y
+    lowest_cost = nil
+    a_pushes = 0
+    until (lowest_cost && cost >= lowest_cost) || dx.negative? || dy.negative?
+      total_cost = pushB(cost, dx, dy)
+      lowest_cost = [total_cost, lowest_cost || Float::INFINITY].min if total_cost
+      cost, dx, dy = pushA(cost, dx, dy)
+      a_pushes += 1
     end
-
-    result = iterate(cost + COST_A, dx - ax, dy - ay, lowest_cost, num_pushes + 1)
-    lowest_cost = [result, lowest_cost || Float::INFINITY].min if result
-    lowest_cost
-  end
-
-  def play
-    # walk(0, x, y, nil, 0) || 0
-    result = iterate(0, x, y, nil, 0)
-    # puts "SCORED: X=#{x}, Y=#{y}, #{result}"
-    result || 0
+    lowest_cost || 0
   end
 end
 
@@ -47,6 +50,7 @@ class Claw
   attr_accessor :machines
 
   DIGITS = /\d+/
+  BIGNUM = 10_000_000_000_000
 
   def self.create(input)
     claw = Claw.new
@@ -64,7 +68,15 @@ class Claw
   end
 
   def part1
-    machines.reduce(0) { |sum, m| sum + m.play }
+    machines.reduce(0) { |sum, m| sum + m.solve }
+  end
+
+  def part2
+    machines.reduce(0) do |sum, m|
+      m.x += BIGNUM
+      m.y += BIGNUM
+      sum + m.solve
+    end
   end
 end
 
@@ -72,3 +84,4 @@ input = IO.readlines(ARGV[0])
 
 claw = Claw.create(input)
 puts claw.part1
+puts claw.part2
